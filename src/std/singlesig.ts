ad1489c371945f770d6310de01f18b86fca2521a
brusher_ru <brusher.designer@gmail.com>
2023-02-03 14:24:39 -0500

    chore: fix type warning

--- src/std/singlesig.ts
+++ src/std/singlesig.ts
@@ -3,7 +3,7 @@ import { Compact64 } from '../codecs/compact';
 import { Address, GasPrice, Nonce, PublicKey } from '../codecs/core';
 import { SingleSig } from '../codecs/signatures';
 import withTemplateAddress from '../codecs/withTemplateAddress';
-import Transaction from '../transaction';
+import Transaction, { Payload } from '../transaction';
 import { bytesToHex } from '../utils/hex';
 import { padAddress } from '../utils/padBytes';
 
@@ -33,7 +33,7 @@ const SpendPayload = Struct({
 });
 
 // Template
-const newT = <T, S>(n: number, pc: Codec<T>, sig: Codec<S>) =>
+const newT = <T extends Payload, S>(n: number, pc: Codec<T>, sig: Codec<S>) =>
   new Transaction({
     address: SINGLE_SIG_TEMPLATE_ADDRESS,
     methodSelector: n,
