96e9d283e2ba5c9d4ebdbdc4cbd4cf66daacc114
brusher_ru <brusher.designer@gmail.com>
2024-07-11 02:24:23 +0300

    tweak: add tests

--- tests/vesting.spec.ts
+++ tests/vesting.spec.ts
@@ -1,36 +1,33 @@
-import MultiSigAccount from '../src/std/multisig';
+import VestingTpl from '../src/std/vesting';
 import { serializeBigInts } from './utils';
 
-describe('MultiSigTemplate', () => {
+describe('VestingTemplate', () => {
   it('principal', () => {
     const pk1 = Uint8Array.from(new Array(32).fill(0));
     const pk2 = Uint8Array.from(new Array(32).fill(1));
-    const tpl = MultiSigAccount.methods[0];
+    const tpl = VestingTpl.methods[0];
     const principal0 = tpl.principal({ Required: 1n, PublicKeys: [pk1] });
     const principal1 = tpl.principal({ Required: 1n, PublicKeys: [pk1, pk2] });
     const principal2 = tpl.principal({ Required: 2n, PublicKeys: [pk1, pk2] });
 
     expect(principal0).toEqual(
       Uint8Array.from([
-        0, 0, 0, 0, 0, 103, 0, 55, 170, 33, 187, 2, 108, 114, 54, 238, 53, 6,
-        37, 92, 141, 232, 87, 147,
+        0, 0, 0, 0, 231, 95, 174, 188, 143, 109, 173, 51, 182, 179, 255, 217, 224, 124, 45, 90, 169, 170, 31, 211
       ])
     );
     expect(principal1).toEqual(
       Uint8Array.from([
-        0, 0, 0, 0, 18, 208, 129, 242, 99, 143, 108, 60, 78, 223, 107, 133, 148,
-        136, 33, 167, 245, 34, 11, 59,
+        0, 0, 0, 0, 88, 148, 144, 14, 42, 122, 93, 94, 166, 72, 155, 132, 184, 29, 214, 51, 77, 3, 50, 111
       ])
     );
     expect(principal2).toEqual(
       Uint8Array.from([
-        0, 0, 0, 0, 33, 55, 229, 172, 159, 13, 197, 85, 167, 212, 25, 209, 104,
-        249, 209, 197, 75, 105, 138, 68,
+        0, 0, 0, 0, 234, 242, 113, 169, 206, 0, 235, 238, 60, 151, 125, 116, 185, 226, 199, 118, 208, 20, 228, 2
       ])
     );
   });
   it('self-spawn', () => {
-    const tpl = MultiSigAccount.methods[0];
+    const tpl = VestingTpl.methods[0];
 
     const pk1 = Uint8Array.from(new Array(32).fill(0));
     const pk2 = Uint8Array.from(new Array(32).fill(1));
@@ -65,12 +62,14 @@ describe('MultiSigTemplate', () => {
       })
     );
   });
-  it('spend', () => {
-    const tpl = MultiSigAccount.methods[16];
+  it('drain', () => {
+    const tpl = VestingTpl.methods[17];
     const pk1 = Uint8Array.from(new Array(32).fill(0));
     const pk2 = Uint8Array.from(new Array(32).fill(1));
     const spawnArgs = { Required: 2n, PublicKeys: [pk1, pk2] };
     const principal = tpl.principal(spawnArgs);
+    const vaultAddr = Uint8Array.from(new Array(24).fill(9));
+    const destnAddr = Uint8Array.from(new Array(24).fill(3));
 
     const fakeSignature = {
       Ref: 0n,
@@ -78,8 +77,9 @@ describe('MultiSigTemplate', () => {
     };
     const payload = {
       Arguments: {
-        Destination: principal,
-        Amount: 120n,
+        Vault: vaultAddr,
+        Destination: destnAddr,
+        Amount: 10n ** 9n, // 1 SMH
       },
       Nonce: 1n,
       GasPrice: 1n,
